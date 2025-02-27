afddc8262a69270402c435c8f2433f9a6acb2258
CORNERSTONE-BB\Waheed.Mukhtar <Waheed.Mukhtar@cornerstone-bb.com>
2025-02-27 12:48:00 -0700

    Insight

--- AZ-Web-APP-01/Pages/Index.cshtml.cs
+++ AZ-Web-APP-01/Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace AZ_Web_APP_01.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,IConfiguration configration)
         {
             _logger = logger;
+            this._configration = configration;
         }
 
         public void OnGet()
         {
-
+            ViewData["Greeting"] = _configration["Greeting"];
         }
     }
 }
